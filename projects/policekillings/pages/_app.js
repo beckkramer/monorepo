ade4a5ca063b6a22d4d3e2d68252594a875acad7
Kamran Rahman <kamranrahman@gmail.com>
2020-10-15 19:29:35 -0700

    Resolving comments!
    Resolving comments!

--- projects/policekillings/pages/_app.js
+++ projects/policekillings/pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
-        <title>Project One</title>
+        <title>Police Killings</title>
         <link rel="shortcut icon" type="image/x-icon" href="https://images.squarespace-cdn.com/content/v1/5e7662fe45630059ae9347cf/1588662098088-YIU53RDZNTCCDI2JU0V4/ke17ZwdGBToddI8pDm48kFHuvyq-Sr_o8e-dvP-jC25Zw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIqfoiNJC-Ls_8uKMOnfDCy5Jt-ds5cksGKcyU-MtDa7Q/favicon.ico?format=100w" />
       </Head>
       <GlpThemeProvider>
