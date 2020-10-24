f014394498b30d825dfe2a67192e509faf68f6b1
Kamran Rahman <kamranrahman@gmail.com>
2020-10-24 13:21:16 -0700

    Reverting the migration I tried and starting from scratch.
    As I was working on migrating the police killings app to the monorepo, I started running into an issue where I was needing to yarn add mysql. However, then I was being prompted to install other dependencies that are internal to mysql. I installed those internal dependencies one by one, until I reached one that wasn't installable (fs). fs doesn't seem to be compatible with the app based on an error message I was seeing in bash. However, the code wouldn't compile or spin up locally due to not having fs installed, so I am reverting all those changes all together for now.

--- projects/policekillings/pages/index.js
+++ projects/policekillings/pages/index.js
@@ -2,70 +2,21 @@ import Head from 'next/head'
 
 import { AppBar, NavLink } from 'components'
 
-import React, { useState, useEffect } from "react";
-// import { Container, Col, Row, Form, Badge } from "react-bootstrap";
-
-import {
-  Box,
-  Chip,
-  Container,
-  Grid,
-  Hidden,
-  Switch,
-  Typography,
-} from "@material-ui/core";
-
-
 import styles from '../styles/Home.module.css'
 
-
 export default function Home() {
-  const universalAPIFetch = async (url) => {
-    if (!process.browser) {
-      const { getApiData } = require("api/routes/appRoutes");
-      return getApiData(url);
-    }
-    const rawData = await fetch(`/api/${url}`);
-    return rawData.json();
-  };
-  
-  const getNationalData = async () => {
-    const shootingsByState = await universalAPIFetch(
-      "count/shootings/state/name"
-    );
-    const populationDataRaw = await fetch(
-      "https://datausa.io/api/data?drilldowns=State&measures=Population&year=latest"
-    );
-    const shootingsOverTime = await universalAPIFetch("count/shootings/overtime");
-    const last20Items = await universalAPIFetch("shootings/last20");
-    const unarmedItems = await universalAPIFetch("count/unarmedKillings");
-    const armedItems = await universalAPIFetch("count/armedKillings");
-    const shootings = await universalAPIFetch("shootings");
-  
-    const topPoliceDepartments = await universalAPIFetch("count/shootings/pd");
-    const populationData = await populationDataRaw.json();
-    const filteredShootingsByState = shootingsByState.filter((state) =>
-      populationData.data.find((s) => s.State === state.state)
-    );
-    const shootingsPerCapita = filteredShootingsByState.map((state) => ({
-      ...state,
-      total:
-        state.total /
-        (populationData.data.find((s) => s.State === state.state).Population /
-          1000000),
-    }));
   return (
     <div className={styles.container}>
       <Head>
-        <title>Police Killings App</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <AppBar
-        projectDescription="An app that reports out on police killing statistics throughout the United States"
+        projectDescription="Police Killings App"
         projectName="Police Killings App"
       >
-        <NavLink href="/one" label="Project One Contact" />
+        <NavLink href="/one" label="Police Killings App" />
       </AppBar>
 
       <main className={styles.main}>
@@ -73,5 +24,4 @@ export default function Home() {
       </main>
     </div>
   )
-};
 }
